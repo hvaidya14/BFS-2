964ee07626c8aa0f468496b0e18d8f271bacf6bd
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-29 08:08:14 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -85,3 +85,58 @@ class Solution {
         dfs(root.left, lvl+1);
     }
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode xparent=null;
+    TreeNode yparent=null;   
+    int xlvl=0;
+    int ylvl=0;     
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+        dfs(root, null, 0, x, y);
+        if (xparent == yparent || xlvl != ylvl) {
+            System.out.println(xparent);
+            System.out.println(yparent);
+            System.out.println(xlvl);
+            System.out.println(ylvl);
+            return false;
+        }
+        return true;
+
+    }
+    private void dfs(TreeNode root, TreeNode parent, int lvl, int x, int y) {
+        if (root == null) {
+            return;
+        }
+        if (xlvl != 0 && ylvl != 0) {
+            return;
+        }
+        if(root.val == x ) {
+            xparent = parent;
+            xlvl=lvl;
+        }
+        if (root.val == y) {
+            yparent = parent;
+            ylvl=lvl;
+        }
+        dfs(root.left, root, lvl+1, x, y);
+        dfs(root.right, root, lvl+1, x, y);
+    }
+}
