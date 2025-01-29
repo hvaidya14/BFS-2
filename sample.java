fea96c836b1bba0fa34860c8750fba444b834974
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-29 07:40:15 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -43,3 +43,45 @@ class Solution {
         return result;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<Integer>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return result;
+        }
+
+        dfs(root, 0);
+        return result;
+    }
+
+    private void dfs(TreeNode root, int lvl) {
+        if (root == null) {
+            return;
+        }
+
+        if (lvl == result.size()) {
+            result.add(root.val);
+        }
+
+        dfs(root.right, lvl+1);
+        dfs(root.left, lvl+1);
+    }
+}
